9c15b988d2e2caaa9e3a07566ab11ce117b0a0e3
Ertuğrul Demir <44205116+ertugruldmr@users.noreply.github.com>
2023-04-20 14:48:52 +0300

    Update app.py

--- demo_app/app.py
+++ demo_app/app.py
@@ -1,56 +1,58 @@
-import gradio as gr
-import tensorflow as tf
-from glob import glob
+import os
+import cv2
 import numpy as np
-import pickle
-import json
+import matplotlib.pyplot as plt
+import gradio as gr
 
-# loading the files
-model_path = "tuned_ResNetV2"
-model = tf.keras.models.load_model(model_path)
-labels = ['dandelion', 'daisy', 'tulips', 'sunflowers', 'roses']
+options = ["eye", "eye_tree_eyeglasses", "frontalcatface", "frontalcatface_extended", 
+            "frontalface_alt", "frontalface_alt2","frontalface_alt_tree",
+            "frontalface_default","fullbody", "lefteye_2splits", "lowerbody",
+            "profileface", "righteye_2splits", "smile", "upperbody"]
 
-# examples
-examples_path = "examples"
 
-def process_image(image):
-    # Convert into tensor
-    image = tf.convert_to_tensor(image)
+def detect(img, option):
 
-    # Cast the image to tf.float32
-    image = tf.cast(image, tf.float32)
-    
-    # Resize the image to img_resize
-    image = tf.image.resize(image, (224,224))
-    
-    # Normalize the image
-    image /= 255.0
-    
-    # Return the processed image and label
-    return image
+  # preparing the corresponding file
+  file_path = f"haarcascade_{option}.xml"
+  valid_model_path = cv2.data.haarcascades + file_path
 
-def predict(image):
+  # preparingt the image
+  old_size = img.shape[:2]
+  img = cv2.resize(img, (400, 600))
+  gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+  green, thickness = (0, 255, 0), 2
 
-  # Pre-procesing the data
-  images = process_image(image)
+  # loading the model
+  haarcascader = cv2.CascadeClassifier(valid_model_path)
 
-  # Batching
-  batched_images = tf.expand_dims(images, axis=0)
+  # detection
+  detections = haarcascader.detectMultiScale(gray)
+
+  # Drawing the ROI
+  for (x, y, w, h) in detections:
+
+    l_t, r_b = (x, y),(x+w, y+h)
+    img = cv2.rectangle(img, l_t, r_b, green, thickness)
+
   
-  prediction = model.predict(batched_images).flatten()
-  confidences = {labels[i]: float(prediction[i]) for i in range(len(labels))}
-  return confidences
+  img = cv2.resize(img, old_size)
 
-# declerating the params
-component_params = {
-    "fn":predict, 
-    "inputs":gr.Image(shape=(224, 224)),
-    "outputs":gr.Label(num_top_classes=len(labels)),
-    "examples":examples_path,
-}
+  return img
 
-# Instantiating example demo app
-demo = gr.Interface(**component_params)
+# Creating the GUI components of demo app
+with gr.Blocks() as demo:
+    
+    # creating the components
+    img = gr.Image(type="numpy", source="upload")
+    option = gr.Dropdown(choices = options, value="frontalface_default", label="Detector")
+    detect_btn = gr.Button("Detect")
+    output = gr.Image(type="numpy")
+
+    # connecting the button functions
+    detect_btn.click(detect, inputs=[img, option], outputs=output)
+
+    # setting the exmples
+    gr.Examples("examples", img)
             
 # Launching the demo
 if __name__ == "__main__":
